1f94ff60800841a45c32f87e215419c1ed25b80b
slilik <halykalu@gmail.com>
2020-09-12 14:07:09 +0400

    Change bresenham algorithm for lab.

--- canvas/bresenhamAlgo/script.js
+++ canvas/bresenhamAlgo/script.js
@@ -3,38 +3,61 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let color = {r: 0, g: 0, b: 0, a: 1}
+var color = {r: 0, g: 0, b: 0, a: 1};
+var points = [];
+var last = 1;
+var drawed = false;
 
 function setPixel(x, y) {
     ctx.fillStyle = "rgba(" + color.r + "," + color.g + "," + color.b + "," + color.a + ")";
     ctx.fillRect(x, y, 1, 1);
 }
 
-var plot = function(x, y) { //кисточка - ставит пиксель своего цвета
+const plot = function(x, y) { //кисточка - ставит пиксель своего цвета
     if(isFinite(x) && isFinite(y)){
         setPixel(x,y);
     }
 };
-var changeColor = function(){
+const changeColor = function(){
     color = {r: Math.floor(Math.random() * 255),
         g: Math.floor(Math.random() * 255),
         b: Math.floor(Math.random() * 255),
         a: 1};
 }
-var points = [];
-last = -1;
+const drawNextLine = function(){
+    color = {r: 255, g: 0, b: 0, a: 1};
+    if(drawed){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawed = false;
+        points = [];
+        last = 1;
+        return;
+    }
+    if(points.length < 2){
+        alert("Недостаточно точек для отрисовки.");
+        return;
+    }
+    if(last === points.length){
+        drawLine(points[last-1].x, points[last-1].y, points[0].x, points[0].y);
+        drawed = true;
+    } else {
+        drawLine(points[last].x, points[last].y, points[last-1].x, points[last-1].y);
+    }
+    last += 1;
+}
 canvas.addEventListener('click', function (evt) {
     points.push({x: evt.clientX, y: evt.clientY});
-    last += 1;
     setPixel(evt.clientX, evt.clientY);
-    if(points.length > 1){
-        drawLine(points[last].x, points[last].y, points[last-1].x, points[last-1].y );
-    }
-    changeColor();
 });
+
 canvas.addEventListener('contextmenu', function(evt){
     evt.preventDefault();
-    changeColor();
+    drawNextLine();
+});
+
+canvas.addEventListener('dblclick', function(evt){
+    evt.preventDefault();
+    drawNextLine();
 });
 
 function drawLine(x1, y1, x2, y2) {
@@ -47,7 +70,6 @@ function drawLine(x1, y1, x2, y2) {
     while (x1 != x2 || y1 != y2) {
         plot(x1, y1);
         var error2 = error * 2;
-        //
         if (error2 > -deltaY) {
             error -= deltaY;
             x1 += signX;
