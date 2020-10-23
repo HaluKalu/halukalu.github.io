230c0c2053a2031f821153140ab11e9882277db2
halukalu <halykalu@gmail.com>
2020-10-23 16:39:01 +0400

    Added simple react site

---
+++ sites/react/simple_site/precache-manifest.1f5c74f5339985df0fb781caa8951552.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5260059573b7a5ee7a6f6ec6426e239",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/index.html"
+  },
+  {
+    "revision": "48963ddf8c007e11909e",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/css/main.649161c3.chunk.css"
+  },
+  {
+    "revision": "76bbd46b8e5dd880b149",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/js/2.4fb3b2d3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/js/2.4fb3b2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48963ddf8c007e11909e",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/js/main.fe705862.chunk.js"
+  },
+  {
+    "revision": "682377bd8253f0b5d3f7",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/js/runtime-main.240993c2.js"
+  },
+  {
+    "revision": "a85d5cf1ea8bb339cf2f71ee1922948a",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/media/back.a85d5cf1.jpeg"
+  },
+  {
+    "revision": "964e4d8417299448d472e9c5c801f614",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/media/simple.964e4d84.svg"
+  },
+  {
+    "revision": "bc9581a831010fdc8882a16cc892be7f",
+    "url": "/home/slilik/prjcts/react-admin/admin/build/static/media/user.bc9581a8.svg"
+  }
+]);
